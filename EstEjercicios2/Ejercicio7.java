f4195ee9d37dec6f17254275a4da1cf065fc302c
derson1444 <>
2021-10-31 16:29:04 +0000

    lgdpsg

---
+++ EstEjercicios2/Ejercicio7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ejercicio_4_9 {
+  static Scanner sc=new Scanner(System.in);
+
+  static void deposito() {
+    //definir variables
+    double cantidad=0;
+    int cantaños=0, contador=1;
+    //datos de entrada
+    System.out.println("***** Ejercicio 7 ******");
+    System.out.println("Ingrese los años que puso dinero:");
+    cantaños=sc.nextInt();
+    //Proceso y datos de salida
+    while (contador<=cantaños) {
+      System.out.println("Indique la cantidad de dinero que ingreso el año "+contador+":");
+      cantidad=sc.nextInt();
+      cantidad=(cantidad*0.1)+cantidad;
+      contador++;
+    }
+    System.out.println("Para el año "+cantaños+" ahorro: "+cantidad);
+  }
+  
+  public static void main(String[] args) {
+    deposito();
+  }
+}
